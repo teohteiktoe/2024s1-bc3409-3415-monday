622ecd4427090b79d3d5a4d0890ac12b8fe1e809
Teoh TeikToe <teohteiktoe@gmail.com>
2024-08-19 15:38:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask,render_template,request
+import google.generativeai as palm
+
+api = "AIzaSyCaQcgKn95ZO6AR1t2PXzk9UydTkt4sWZQ"
+palm.configure(api_key=api)
+model = {"model": "models/chat-bison-001"}
 
 app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
     return(render_template("index.html"))
+
+@app.route("/financial_FAQ",methods=["GET","POST"])
+def financial_FAQ():
+    return(render_template("financial_FAQ.html"))
+
+@app.route("/makersuite",methods=["GET","POST"])
+def makersuite():
+    q = request.form.get("q")
+    r = palm.chat(messages=q, **model)
+    return(render_template("makersuite.html",r=r.last))
 
 if __name__ == "__main__":
     app.run()
