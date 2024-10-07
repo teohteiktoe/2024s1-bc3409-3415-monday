177aacf45c7b64998c2be4cdc16544237f9f8b32
Teoh TeikToe <teohteiktoe@gmail.com>
2024-10-07 16:38:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask,render_template,request
 import google.generativeai as genai
-import os
+import textblob
 
-api = os.getenv("MAKERSUITE")
+api = ""
 genai.configure(api_key=api)
 model = genai.GenerativeModel("gemini-1.5-flash")
 
@@ -21,6 +21,16 @@ def makersuite():
     q = request.form.get("q")
     r = model.generate_content(q)
     return(render_template("makersuite.html",r=r.text))
+
+@app.route("/sentiment_analysis",methods=["GET","POST"])
+def sentiment_analysis():
+    return(render_template("sentiment_analysis.html"))
+
+@app.route("/sentiment_analysis_result",methods=["GET","POST"])
+def sentiment_analysis_result():
+    q = request.form.get("q")
+    r=textblob.TextBlob(q).sentiment
+    return(render_template("sentiment_analysis_result.html",r=r))
 
 if __name__ == "__main__":
     app.run()
